a2feccc7a62a86b717321c4f7e128a4e38f466d9
Anany More <anany.more@ananyamore-vrs-13.successivetech.com>
2025-09-09 16:58:50 +0530

    removed dist and .env

--- dist/middleware/customHeader.js
+++
@@ -1,8 +0,0 @@
-const customHeader = (headerName, headerValue) => {
-    return (req, res, next) => {
-        res.setHeader(headerName, headerValue);
-        next();
-    };
-};
-export default customHeader;
-//# sourceMappingURL=customHeader.js.map
