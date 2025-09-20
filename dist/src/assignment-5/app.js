a0ac58ed24f7f078b0f9469f0a52abf854d174b9
Anany More <anany.more@ananyamore-vrs-13.successivetech.com>
2025-09-20 12:48:05 +0530

    removed te dist

--- dist/src/assignment-5/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// 5.Create an asynchronous route that intentionally throws an error.Implement error handling for asynchronous errors within the route.Send an appropriate response to the client, indicating the error.
-// I have implemented the user stories in all application and globaly. 
-//# sourceMappingURL=app.js.map
--- dist/src/middleware/requestLogger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const requestLogger = (req, res, next) => {
-    const { url, method } = req;
-    console.log(`[${new Date().toLocaleTimeString()}] ${method} ${url}`);
-    next();
-};
-exports.default = requestLogger;
-//# sourceMappingURL=requestLogger.js.map
